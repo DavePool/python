15a77204c7ac82b1baf61ac4ac67353f676eceb4
davepool <you@example.com>
2023-03-17 13:28:10 -0600

    new examples

---
+++ collatz_sequence.py
@@ -0,0 +1,30 @@
+#/usr/bin/python3
+
+
+def collatz(number):
+
+    print(number) 
+    if number % 2 == 0:
+        return collatz( number / 2)
+    elif number == 1:
+        return 1
+    else:
+        return collatz(number * 3 +1) 
+
+def brute(number):
+
+    while True:
+
+        if number % 2 == 0:
+            number = number / 2
+            print(number)
+        elif number == 1:
+            break
+        else:
+            number = (number * 3) + 1
+            print(number)
+
+print (collatz(1000000))
+
+print (brute(1000000))
+
