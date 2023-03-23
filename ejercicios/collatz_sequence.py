b5e70556174c62a0e22f11cce28f461f84e1cdba
davepool <you@example.com>
2023-03-23 17:08:37 -0600

    collats new config

--- ejercicios/collatz_sequence.py
+++ ejercicios/collatz_sequence.py
@@ -1,30 +1,48 @@
 #/usr/bin/python3
 
+import unittest
 
 def collatz(number):
 
     print(number) 
     if number % 2 == 0:
-        return collatz( number / 2)
+        return int(collatz( number / 2))
     elif number == 1:
-        return 1
+        return int(1)
     else:
-        return collatz(number * 3 +1) 
+        return int(collatz(number * 3 +1)) 
 
 def brute(number):
+    if number <= 0:
+        raise ValueError("Only positive integers are allowed")
+    else:
+        stp = 0
+        while True:
 
-    while True:
+            if number % 2 == 0:
+                stp +=  1
+                number = number / 2
+                print("Step number: "+str(stp)+" value: "+str(int(number)))
+            elif number == 1:
+                return (stp)
+            else:
+                stp += 1
+                number = (number * 3) + 1
+                print("Step number: "+str(stp)+" value: "+str(int(number)))
 
-        if number % 2 == 0:
-            number = number / 2
-            print(number)
-        elif number == 1:
-            break
-        else:
-            number = (number * 3) + 1
-            print(number)
 
-print ("Enter a number: ")
+# print ("Enter a number: ")
+# collatz( int(input()))
 
-collatz( int(input()))
+# unit test for collatz steps
+class CollatzConjectureTest(unittest.TestCase):
 
+    def test_zero_steps_for_one(self):
+        self.assertEqual(brute(1), 0)
+    def test_divide_if_even(self):
+
+        self.assertEqual(brute(12), 9)
+    def test_even_and_odd_steps(self):
+        self.assertEqual(brute(4), 2)
+
+unittest.main()
