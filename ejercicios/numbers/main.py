0973cc544b1427c74504fa57927dd32e795f2ca0
davepool <you@example.com>
2023-03-23 15:27:00 -0600

    python exercices

---
+++ ejercicios/numbers/main.py
@@ -0,0 +1,29 @@
+import exchange
+
+print("******** Exchange exercese *******************")
+
+print("Exchange money: "+str(exchange.exchange_money(127.5,1.2)))
+print("Change money: "+str(exchange.get_change(127.5,120)))
+print("Value of bills: "+str(exchange.get_value_of_bills(5,128)))
+print("get of bills: "+str(exchange.get_number_of_bills(127.5,5)))
+print("get leftover of bills: "+str(exchange.get_leftover_of_bills(127.5,20)))
+print("\n")
+
+inputs = [
+    (100000, 10.61, 10, 1),
+    (1500, 0.84, 25, 40),
+    (470000, 1050, 30, 10000000000),
+    (470000, 0.00000009, 30, 700),
+    (425.33, 0.0009, 30, 700)]
+
+for values in inputs:
+    print("\nbudget: " + str(values[0]) +
+          "\nExchange rate: "+ str(values[1]) +
+          "\nSpread: "+ str(values[2]) + 
+          "\nDenomination: " + str(values[3]))
+
+    print("Exchangeable value: "+str(exchange.exchangeable_value(values[0], values[1], values[2], values[3])))
+
+#print("Exchangeable value: "+str(exchange.exchangeable_value(470000, 0.00000009, 30, 700)))
+
+print("******** End of Exchange exercese ************")
