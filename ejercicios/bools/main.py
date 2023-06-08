be1c9ff57c12a3d5715c3ffe00e5a87f48eda43c
davepool <you@example.com>
2023-06-08 08:08:29 -0600

    arcade game

---
+++ ejercicios/bools/main.py
@@ -0,0 +1,9 @@
+import arcade_game
+
+
+print ("************ Arcade game ********************")
+print("I can eat a gost?: "+str(arcade_game.eat_ghost(True, True)))
+print("My score is: "+str(arcade_game.score(True, True)))
+print("I lose ?: "+str(arcade_game.lose(True, True)))
+
+print ("************ End of arcade game *************")
